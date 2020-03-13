a24c3b55914fce8626930abaad251069ba0b98aa
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:44:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,18 +16,6 @@ class Triangle
       return :scalene
     end
 
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
-
   end
 
   class TriangleError < StandardError
