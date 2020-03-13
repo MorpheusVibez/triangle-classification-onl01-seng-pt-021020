1dfac24d6e3cc1ca49b4e37a3794de1d672b3f49
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:08:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  # attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(side_1, side_2, side_3)
+    @triangles = []
+    @triangles << side_1
+    @triangles << side_2
+    @triangles << side_3
+  end
+
+  def kind
+    if @triangles.each {|side| side.even?}
+      :equilateral
+    elsif @triangles.first(2) == @triangles.first(2)
+      :isosceles
+
+
+    end
+
+
+  end
+
+  # class TriangleError < StandardError
+  #
+  # end
 end
