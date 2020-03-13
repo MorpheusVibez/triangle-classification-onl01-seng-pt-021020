d690c1d2b25782ec8314b8694532c5a059ccd650
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:49:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,13 +22,13 @@ class Triangle
       raise TriangleError
     end
 
-    if @side_1 <= 0 || @side_2 <= 0 ||@side_3 <= 0
-      raise TriangleError
-    end
-
-    if @side_1 == nil || @side_2 == nil ||@side_3 == nil
-      raise TriangleError
-    end
+    # if @side_1 <= 0 || @side_2 <= 0 ||@side_3 <= 0
+    #   raise TriangleError
+    # end
+    #
+    # if @side_1 == nil || @side_2 == nil ||@side_3 == nil
+    #   raise TriangleError
+    # end
 
   end
 
