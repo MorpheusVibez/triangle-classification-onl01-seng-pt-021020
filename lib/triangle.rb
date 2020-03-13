3a6862b8baf6f16f7991ddab00c65eb275a24d8e
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:45:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,21 @@ class Triangle
   # attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(side_1, side_2, side_3)
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-    raise TriangleError
   end
 
   def kind
