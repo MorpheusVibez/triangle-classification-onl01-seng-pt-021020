6c380fe9f7784f6c1aa44d7b65971f16b142200e
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:26:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,14 @@ class Triangle
   end
 
   def kind
-
+    if @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
 
   class TriangleError < StandardError
 
