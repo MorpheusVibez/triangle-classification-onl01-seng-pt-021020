4f0f5be30bc58612ebd8edb54b374295ece985aa
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:16:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,21 +2,20 @@ class Triangle
   # attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(side_1, side_2, side_3)
-    @triangles = []
-    @triangles << side_1
-    @triangles << side_2
-    @triangles << side_3
+
+    side_1 = side_1
+    side_2 = side_2
+    side_3 = side_3
   end
 
   def kind
-    if @triangles.each {|side| side.even?}
-      :equilateral
-    end
-    if @triangles.first(2) == @triangles.first(2)
-      :isosceles
-
-
-    end
+    if (@side_1 == @side_2) && (@side_2 == @side_3)
+       :equilateral
+     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+       :isosceles
+     elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+       :scalene
+     end
 
 
   end
