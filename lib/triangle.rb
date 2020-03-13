0eebc3b57295a3e6b409c45e4b311d4a5c558e51
Andre Skeene <halodre24@gmail.com>
2020-03-13 18:49:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,15 +18,15 @@ class Triangle
       return :scalene
     end
 
-    if side_1 >= (side_2 + side_3) || side_2 >= (side_1 + side_3) || side_3 >= (side_1 + side_2)
+    if @side_1 >= (@side_2 + @side_3) || @side_2 >= (@side_1 + @side_3) || @side_3 >= (@side_1 + @side_2)
       raise TriangleError
     end
 
-    if side_1 <= 0 || side_2 <= 0 ||side_3 <= 0
+    if @side_1 <= 0 || @side_2 <= 0 ||@side_3 <= 0
       raise TriangleError
     end
-    
-    if side_1 == nil || side_2 == nil ||side_3 == nil
+
+    if @side_1 == nil || @side_2 == nil ||@side_3 == nil
       raise TriangleError
     end
 
